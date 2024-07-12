e264d9e036c170d3ed835102c7914e3f54906e9e
Codgm <sgm0209@naver.com>
2024-07-12 15:47:59 +0900

    feat - 매인 화면 레이아웃 구성

--- src/app/homepage/layout.js
+++ src/app/homepage/layout.js
@@ -3,7 +3,7 @@
 export default function Layout({ children }) {
   return (
     <html>
-      <body leftmargin="0"  topmargin="0">
+      <body leftmargin="0"  topmargin="0" className="mx-auto min-h-screen items-center justify-center">
           {children}
       </body>
     </html>
