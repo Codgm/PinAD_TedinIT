c6af5d22607a82c70601bb82def7e999c42025d8
DOGJHDOG <junhyung031115@gmail.com>
2024-07-15 10:35:27 +0900

    feat - nav바 제작중

--- src/app/homepage/layout.js
+++ src/app/homepage/layout.js
@@ -1,9 +1,11 @@
+import Navbar from "../components/nav";
 
 
 export default function Layout({ children }) {
   return (
     <html>
       <body leftmargin="0"  topmargin="0" className="mx-auto min-h-screen items-center justify-center">
+          <Navbar/>
           {children}
       </body>
     </html>
