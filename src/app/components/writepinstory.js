1cf9008d5c35c3a598432dc2085adc5e65751a03
Codgm <sgm0209@naver.com>
2024-07-16 11:59:41 +0900

    글 작성 플랫폼 선택 부분 수정

--- src/app/components/writepinstory.js
+++ src/app/components/writepinstory.js
@@ -1,4 +1,8 @@
+"use client";
 import React, { useState } from 'react';
+import InstagramIcon from '/public/instagram_icon.svg';
+import YouTubeIcon from '/public/youtube_icon.svg';
+import TikTokIcon from '/public/tiktok_logo_brand_icon.svg';
 
 export default function WritePinStory({ isOpen, closeModal, addPin }) {
   const [content, setContent] = useState('');
@@ -7,18 +11,20 @@ export default function WritePinStory({ isOpen, closeModal, addPin }) {
   const [platforms, setPlatforms] = useState([]);
   const [profilePublic, setProfilePublic] = useState(false);
   const [platformLinks, setPlatformLinks] = useState({});
+  const [showPlatformSelection, setShowPlatformSelection] = useState(false);
 
   const handleContentChange = (e) => setContent(e.target.value);
   const handleImageChange = (e) => setImage(e.target.files[0]);
   const handleShortVideoChange = (e) => setShortVideo(e.target.files[0]);
-  const handlePlatformChange = (e) => {
-    const platform = e.target.value;
+  
+  const togglePlatform = (platform) => {
     if (platforms.includes(platform)) {
       setPlatforms(platforms.filter((p) => p !== platform));
     } else {
       setPlatforms([...platforms, platform]);
     }
   };
+
   const toggleProfileVisibility = () => setProfilePublic(!profilePublic);
 
   const handlePost = () => {
@@ -37,6 +43,10 @@ export default function WritePinStory({ isOpen, closeModal, addPin }) {
   const handlePlatformLinkChange = (e, platform) => {
     const value = e.target.value;
     setPlatformLinks({ ...platformLinks, [platform]: value });
+  };
+
+  const togglePlatformSelection = () => {
+    setShowPlatformSelection(!showPlatformSelection);
   };
 
   if (!isOpen) return null;
@@ -76,74 +86,69 @@ export default function WritePinStory({ isOpen, closeModal, addPin }) {
             />
           </div>
           <div className="space-y-2">
-            <label className="block mb-2 text-sm text-gray-600">플랫폼 선택</label>
-            <div className="space-y-2">
-              <label className="flex items-center">
-                <input
-                  type="checkbox"
-                  className="mr-2"
-                  value="instagram"
-                  checked={platforms.includes('instagram')}
-                  onChange={handlePlatformChange}
-                />
-                <span className="text-sm">
-                    <InstagramIcon className="w-6 h-6 mr-2" /> Instagram
-                </span>
-              </label>
-              {platforms.includes('instagram') && (
-                <input
-                  type="text"
-                  className="w-full border p-2 rounded-md"
-                  placeholder="Instagram URL"
-                  value={platformLinks['instagram'] || ''}
-                  onChange={(e) => handlePlatformLinkChange(e, 'instagram')}
-                />
-              )}
+            <button
+              className="block mb-2 text-sm text-gray-900 flex items-center"
+              onClick={togglePlatformSelection}
+            >
+              플랫폼
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-4 h-4 ml-1">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg>
+            </button>
+            {showPlatformSelection && (
+              <div className="space-y-2">
+                <button
+                  className={`flex items-center ${platforms.includes('instagram') ? 'bg-gray-200' : 'bg-white'}`}
+                  onClick={() => togglePlatform('instagram')}
+                >
+                  <InstagramIcon className="w-6 h-6 mr-2" />
+                  <span className="text-sm">Instagram</span>
+                </button>
+                {platforms.includes('instagram') && (
+                  <input
+                    type="text"
+                    className="w-full border p-2 rounded-md"
+                    placeholder="Instagram URL"
+                    value={platformLinks['instagram'] || ''}
+                    onChange={(e) => handlePlatformLinkChange(e, 'instagram')}
+                  />
+                )}
 
-              <label className="flex items-center">
-                <input
-                  type="checkbox"
-                  className="mr-2"
-                  value="youtube"
-                  checked={platforms.includes('youtube')}
-                  onChange={handlePlatformChange}
-                />
-                <span className="text-sm">
-                    <YouTubeIcon className="w-6 h-6 mr-2" /> YouTube
-                </span>
-              </label>
-              {platforms.includes('youtube') && (
-                <input
-                  type="text"
-                  className="w-full border p-2 rounded-md"
-                  placeholder="YouTube URL"
-                  value={platformLinks['youtube'] || ''}
-                  onChange={(e) => handlePlatformLinkChange(e, 'youtube')}
-                />
-              )}
+                <button
+                  className={`flex items-center ${platforms.includes('youtube') ? 'bg-gray-200' : 'bg-white'}`}
+                  onClick={() => togglePlatform('youtube')}
+                >
+                  <YouTubeIcon className="w-6 h-6 mr-2" />
+                  <span className="text-sm">YouTube</span>
+                </button>
+                {platforms.includes('youtube') && (
+                  <input
+                    type="text"
+                    className="w-full border p-2 rounded-md"
+                    placeholder="YouTube URL"
+                    value={platformLinks['youtube'] || ''}
+                    onChange={(e) => handlePlatformLinkChange(e, 'youtube')}
+                  />
+                )}
 
-              <label className="flex items-center">
-                <input
-                  type="checkbox"
-                  className="mr-2"
-                  value="tiktok"
-                  checked={platforms.includes('tiktok')}
-                  onChange={handlePlatformChange}
-                />
-                <span className="text-sm">
-                    <TikTokIcon className="w-6 h-6 mr-2" /> TikTok
-                </span>
-              </label>
-              {platforms.includes('tiktok') && (
-                <input
-                  type="text"
-                  className="w-full border p-2 rounded-md"
-                  placeholder="TikTok URL"
-                  value={platformLinks['tiktok'] || ''}
-                  onChange={(e) => handlePlatformLinkChange(e, 'tiktok')}
-                />
-              )}
-            </div>
+                <button
+                  className={`flex items-center ${platforms.includes('tiktok') ? 'bg-gray-200' : 'bg-white'}`}
+                  onClick={() => togglePlatform('tiktok')}
+                >
+                  <TikTokIcon className="w-6 h-6 mr-2" />
+                  <span className="text-sm">TikTok</span>
+                </button>
+                {platforms.includes('tiktok') && (
+                  <input
+                    type="text"
+                    className="w-full border p-2 rounded-md"
+                    placeholder="TikTok URL"
+                    value={platformLinks['tiktok'] || ''}
+                    onChange={(e) => handlePlatformLinkChange(e, 'tiktok')}
+                  />
+                )}
+              </div>
+            )}
           </div>
           <div className="space-y-2">
             <label className="flex items-center">
