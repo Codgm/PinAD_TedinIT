89a84d9a5c2842d754a1e5c12edd148576813e97
Codgm <sgm0209@naver.com>
2024-07-30 17:14:52 +0900

    feat 광고 템플릿 전체적으로 수정함. 정기 결제 부분은 아직 완성 못함

--- src/app/components/writepinstory.js
+++ src/app/components/writepinstory.js
@@ -7,8 +7,7 @@ import ThreadsIcon from '/public/threads_icon.svg';
 import { FaCamera, FaPen, FaTags, FaShare, FaCheck, FaImage, FaVideo } from 'react-icons/fa';
 import { MdKeyboardArrowRight } from "react-icons/md";
 import usePostCreation from '@/app/components/logic/writeSeq';
-
-const defaultTags = ['팝니다', '삽니다', '고칩니다', '알립니다'];
+import AdStepComponent from './AdStepComponent';
 
 const steps = [
   { icon: <FaCamera />, label: '미디어' },
@@ -39,15 +38,21 @@ export default function WritePinStory({ isOpen, closeModal, addPin }) {
     setMonetize,
     setAdOption,
     setBoost,
+    setStock,
+    setRadius,
+    setLocation,
+    setPayment,
+    setSelectedCategory,
+    setBundleNValue,
+    setBundleType,
+    setPaymentMode
   } = usePostCreation();
 
   const [selectedDefaultTags, setSelectedDefaultTags] = useState([]);
-  const [bundleType, setBundleType] = useState('');
   const [newTags, setNewTags] = useState([]);
   const [showMonetizeModal, setShowMonetizeModal] = useState(false);
   const [showConfirmModal, setShowConfirmModal] = useState(false);
   const [industryType, setIndustryType] = useState('');
-  const [selectedCategory, setSelectedCategory] = useState('');
 
   const fileInputRef = useRef(null);
 
@@ -158,6 +163,17 @@ export default function WritePinStory({ isOpen, closeModal, addPin }) {
     setIndustryType('');
   };
 
+  const handleAdOptionChange = (event) => {
+    setAdOption(event.target.value);
+  };
+
+  const handlePaymentModeChange = (e) => {
+    setPaymentMode((prevState) => ({
+      ...prevState,
+      paymentMode: e.target.value,
+    }));
+  };
+  
 
   const renderStepIndicator = () => (
     <div className="flex mb-6 justify-start overflow-y-auto">
@@ -282,201 +298,164 @@ export default function WritePinStory({ isOpen, closeModal, addPin }) {
                 />
               </div>
             )}
-            {state.type === '광고' && (
-            <div className="space-y-4">
-              <select
-                className="w-full border rounded-lg p-2"
-                value={selectedCategory}
-                onChange={handleCategoryChange}
-              >
-                <option value="">선택</option>
-                <option value="유통">유통</option>
-                <option value="요식업">요식업</option>
-                <option value="이동형 판매">이동형 판매</option>
-                <option value="서비스업">서비스업</option>
-              </select>
-              {selectedCategory === '유통' && (
-                <div className="space-y-4">
-                  <input
-                    type="text"
-                    placeholder="상품명 입력"
-                    onChange={(e) => setIndustryType(e.target.value)}
-                    className="w-full h-12 border rounded-lg p-2"
-                  />
-                  <input
-                    type="number"
-                    placeholder="재고 수량 입력"
-                    onChange={(e) => setStock(e.target.value)}
-                    className="w-full h-12 border rounded-lg p-2"
-                  />
-                  <div className="flex flex-col space-y-4">
-                    <label className="text-sm">할인 형태 선택</label>
-                    <select onChange={(e) => setDiscountType(e.target.value)} className="w-full border rounded-lg p-2">
-                      <option value="">선택</option>
-                      <option value="단순 할인">단순 할인</option>
-                      <option value="묶음 할인">묶음 할인</option>
-                    </select>
-                  </div>
-                  {state.discountType === '단순 할인' && (
-                    <input
-                      type="text"
-                      placeholder="할인율 또는 할인가 입력"
-                      onChange={(e) => setDiscountValue(e.target.value)}
-                      className="w-full h-12 border rounded-lg p-2"
-                    />
-                  )}
-                  {state.discountType === '묶음 할인' && (
-                    <div>
-                      <label className="text-sm">묶음 할인 유형 선택</label>
-                      <select onChange={(e) => setBundleType(e.target.value)} className="w-full border rounded-lg p-2">
-                        <option value="">선택</option>
-                        <option value="1+1">1+1</option>
-                        <option value="2+1">2+1</option>
-                        <option value="N+1">N+1</option>
-                      </select>
-                      {bundleType === '1+1' && (
-                        <input
-                          type="text"
-                          placeholder="1+1 설정"
-                          onChange={(e) => setBundleDiscountValue(e.target.value)}
-                          className="w-full h-12 border rounded-lg p-2"
-                        />
-                      )}
-                      {bundleType === '2+1' && (
-                        <input
-                          type="text"
-                          placeholder="2+1 설정"
-                          onChange={(e) => setBundleDiscountValue(e.target.value)}
-                          className="w-full h-12 border rounded-lg p-2"
-                        />
-                      )}
-                      {bundleType === 'N+1' && (
-                        <input
-                          type="text"
-                          placeholder="N값 입력 및 설정"
-                          onChange={(e) => setBundleDiscountValue(e.target.value)}
-                          className="w-full h-12 border rounded-lg p-2"
-                        />
-                      )}
-                    </div>
-                  )}
-                  <input
-                    type="text"
-                    placeholder="할인 기간 설정"
-                    className="w-full h-12 border rounded-lg p-2"
-                  />
-              </div>
-              )}
-            </div>
-          )}
+            { <AdStepComponent
+                state={state}
+                setStep={setStep}
+                handleCategoryChange={handleCategoryChange}
+                setSelectedCategory={setSelectedCategory}
+                setIndustryType={setIndustryType}
+                setStock={setStock}
+                setDiscountType={setDiscountType}
+                setDiscountValue={setDiscountValue}
+                setBundleType={setBundleType}
+                setBundleDiscountValue={setBundleDiscountValue}
+                setBundleNValue = {setBundleNValue}
+                handleAdOptionChange={handleAdOptionChange}
+                setRadius={setRadius}
+                setBoost={setBoost}
+                setLocation={setLocation}
+                setPayment={setPayment}
+                handleDefaultTagClick={handleDefaultTagClick}
+                handleNewTagInputKeyDown={handleNewTagInputKeyDown}
+                removeNewTag={removeNewTag}
+                newTags={newTags}
+                selectedDefaultTags={selectedDefaultTags}
+                handlePlatformLinkChange={handlePlatformLinkChange}
+                handlePaymentModeChange = {handlePaymentModeChange}
+              />
+            }
           </div>
         );
       case 3:
         return (
-          state.type === '광고' && (
-            <div className="px-4 md:px-6 space-y-4">
-              <div className="flex flex-col space-y-4">
-                <label className="text-sm">광고 옵션 설정</label>
-                <select onChange={(e) => setAdOption(e.target.value)} className="w-full border rounded-lg p-2">
-                  <option value="">선택</option>
-                  <option value="광고1">광고1</option>
-                  <option value="광고2">광고2</option>
-                </select>
-                <input
-                  type="text"
-                  placeholder="알림 반경 선택"
-                  onChange={(e) => setRadius(e.target.value)}
-                  className="w-full h-12 border rounded-lg p-2"
-                />
-                <label className="text-sm">광고 부스트 사용 여부</label>
-                <select onChange={(e) => setBoost(e.target.value)} className="w-full border rounded-lg p-2">
-                  <option value="">선택</option>
-                  <option value="사용">사용</option>
-                  <option value="사용 안함">사용 안함</option>
-                </select>
-                <input
-                  type="text"
-                  placeholder="지도에서 위치 선택"
-                  onChange={(e) => setLocation(e.target.value)}
-                  className="w-full h-12 border rounded-lg p-2"
-                />
-                <input
-                  type="text"
-                  placeholder="결제 정보 입력"
-                  onChange={(e) => setPayment(e.target.value)}
-                  className="w-full h-12 border rounded-lg p-2"
-                />
-              </div>
-            </div>
-          )
+          <AdStepComponent
+            state={state}
+            setStep={setStep}
+            handleCategoryChange={handleCategoryChange}
+            setSelectedCategory={setSelectedCategory}
+            setIndustryType={setIndustryType}
+            setStock={setStock}
+            setDiscountType={setDiscountType}
+            setDiscountValue={setDiscountValue}
+            setBundleType={setBundleType}
+            setBundleNValue = {setBundleNValue}
+            setBundleDiscountValue={setBundleDiscountValue}
+            handleAdOptionChange={handleAdOptionChange}
+            setRadius={setRadius}
+            setBoost={setBoost}
+            setLocation={setLocation}
+            setPayment={setPayment}
+            handleDefaultTagClick={handleDefaultTagClick}
+            handleNewTagInputKeyDown={handleNewTagInputKeyDown}
+            removeNewTag={removeNewTag}
+            newTags={newTags}
+            selectedDefaultTags={selectedDefaultTags}
+            handlePlatformLinkChange={handlePlatformLinkChange}
+            handlePaymentModeChange = {handlePaymentModeChange}
+          />
         );
       case 4:
         return (
-          <div className="space-y-4">
-          <div className="space-y-2">
-            <label className="block mb-2 text-sm text-gray-600">태그 (최대 10개)</label>
-            <div className="flex flex-wrap">
-              {defaultTags.map(tag => (
-                <button
-                  key={tag}
-                  className={`p-2 m-1 rounded-md ${selectedDefaultTags.includes(tag) ? 'bg-purple-500 text-white' : 'bg-gray-200 text-gray-700'} focus:outline-none`}
-                  onClick={() => handleDefaultTagClick(tag)}
-                >
-                  {tag}
-                </button>
-              ))}
-            </div>
-            <div className="flex flex-wrap ">
-              {newTags.map(tag => (
-                <div key={tag} className="flex items-center bg-purple-500 text-white p-2 m-1 rounded-md">
-                  <span>{tag}</span>
-                  <button onClick={() => removeNewTag(tag)} className="ml-2 text-xs">&times;</button>
-                </div>
-              ))}
-            </div>
-            <input
-              type="text"
-              className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 mt-2"
-              placeholder="새 태그를 입력하고 Enter 키를 누르세요"
-              onKeyDown={handleNewTagInputKeyDown}
-            />
-          </div>
-        </div>
+          <AdStepComponent
+            state={state}
+            setStep={setStep}
+            handleCategoryChange={handleCategoryChange}
+            setSelectedCategory={setSelectedCategory}
+            setIndustryType={setIndustryType}
+            setStock={setStock}
+            setDiscountType={setDiscountType}
+            setDiscountValue={setDiscountValue}
+            setBundleType={setBundleType}
+            setBundleNValue = {setBundleNValue}
+            setBundleDiscountValue={setBundleDiscountValue}
+            handleAdOptionChange={handleAdOptionChange}
+            setRadius={setRadius}
+            setBoost={setBoost}
+            setLocation={setLocation}
+            setPayment={setPayment}
+            handleDefaultTagClick={handleDefaultTagClick}
+            handleNewTagInputKeyDown={handleNewTagInputKeyDown}
+            removeNewTag={removeNewTag}
+            newTags={newTags}
+            selectedDefaultTags={selectedDefaultTags}
+            handlePlatformLinkChange={handlePlatformLinkChange}
+            handlePaymentModeChange = {handlePaymentModeChange}
+          />
+        //   <div className="space-y-4">
+        //   <div className="space-y-2">
+        //     <label className="block mb-2 text-sm text-gray-600">태그 (최대 10개)</label>
+        //     <div className="flex flex-wrap">
+        //       {defaultTags.map(tag => (
+        //         <button
+        //           key={tag}
+        //           className={`p-2 m-1 rounded-md ${selectedDefaultTags.includes(tag) ? 'bg-purple-500 text-white' : 'bg-gray-200 text-gray-700'} focus:outline-none`}
+        //           onClick={() => handleDefaultTagClick(tag)}
+        //         >
+        //           {tag}
+        //         </button>
+        //       ))}
+        //     </div>
+        //     <div className="flex flex-wrap ">
+        //       {newTags.map(tag => (
+        //         <div key={tag} className="flex items-center bg-purple-500 text-white p-2 m-1 rounded-md">
+        //           <span>{tag}</span>
+        //           <button onClick={() => removeNewTag(tag)} className="ml-2 text-xs">&times;</button>
+        //         </div>
+        //       ))}
+        //     </div>
+        //     <input
+        //       type="text"
+        //       className="w-full p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500 mt-2"
+        //       placeholder="새 태그를 입력하고 Enter 키를 누르세요"
+        //       onKeyDown={handleNewTagInputKeyDown}
+        //     />
+        //   </div>
+        // </div>
         );
       case 5:
         return (
-          <div className="space-y-4">
-            <label className="block mb-2 text-sm text-gray-600">SNS 공유</label>
-            <div className="flex space-x-2">
-              <button className="p-2 rounded bg-yellow-300 text-white"><KakaoTalkIcon className="w-6 h-6" /></button>
-              <button className="p-2 rounded bg-green-300 text-white"><LineIcon className="w-6 h-6" /></button>
-              <button className="p-2 rounded bg-pink-300 text-white"><InstagramIcon className="w-6 h-6" /></button>
-              <button className="p-2 rounded bg-gray-300 text-white"><ThreadsIcon className="w-6 h-6" /></button>
-            </div>
-            <div className="flex flex-col space-y-2">
-              <label className="block text-sm text-gray-600">YouTube 프로필</label>
-              <input
-                type="text"
-                className="w-full border p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
-                placeholder="YouTube URL"
-                value={state.platformLinks['youtube'] || ''}
-                onChange={(e) => handlePlatformLinkChange(e, 'youtube')}
-              />
-              <label className="block text-sm text-gray-600">Instagram 프로필</label>
-              <input
-                type="text"
-                className="w-full border p-2 rounded-md focus:outline-none focus:ring-2 focus:ring-purple-500"
-                placeholder="Instagram URL"
-                value={state.platformLinks['instagram'] || ''}
-                onChange={(e) => handlePlatformLinkChange(e, 'instagram')}
-              />
-            </div>
-          </div>
+          <AdStepComponent
+            state={state}
+            setStep={setStep}
+            handleCategoryChange={handleCategoryChange}
+            setSelectedCategory={setSelectedCategory}
+            setIndustryType={setIndustryType}
+            setStock={setStock}
+            setDiscountType={setDiscountType}
+            setDiscountValue={setDiscountValue}
+            setBundleType={setBundleType}
+            setBundleNValue = {setBundleNValue}
+            setBundleDiscountValue={setBundleDiscountValue}
+            handleAdOptionChange={handleAdOptionChange}
+            setRadius={setRadius}
+            setBoost={setBoost}
+            setLocation={setLocation}
+            setPayment={setPayment}
+            handleDefaultTagClick={handleDefaultTagClick}
+            handleNewTagInputKeyDown={handleNewTagInputKeyDown}
+            removeNewTag={removeNewTag}
+            newTags={newTags}
+            selectedDefaultTags={selectedDefaultTags}
+            handlePlatformLinkChange={handlePlatformLinkChange}
+            handlePaymentModeChange = {handlePaymentModeChange}
+          />
         );
       default:
         return null;
     }
   };
+
+  const handleNextStep = () => {
+    if (state.step === 3 && state.type === '광고') {
+      const confirmation = window.confirm('광고를 실행하시겠습니까?');
+      if (confirmation) {
+        setStep(4);
+      }
+    } else {
+      setStep(state.step < 5 ? state.step + 1 : 1);
+    }
+  };
+  
 
   const renderStepButtons = () => {
     return (
@@ -489,7 +468,13 @@ export default function WritePinStory({ isOpen, closeModal, addPin }) {
           이전
         </button>
         <button
-          onClick={() => state.step < 5 ? setStep(state.step + 1) : handlePost()}
+          onClick={() => {
+            if (state.step === 3 && state.type === '광고') {
+              handleNextStep();
+            }else {
+              state.step < 5 ? setStep(state.step + 1) : handlePost();
+            }
+          }}
           className="px-4 py-2 bg-purple-500 text-white rounded hover:bg-purple-600 transition-colors duration-300"
         >
           {state.step < 5 ? '다음' : '완료'}
