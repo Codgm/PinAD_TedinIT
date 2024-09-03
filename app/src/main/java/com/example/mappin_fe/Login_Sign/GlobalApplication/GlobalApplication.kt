3062777eec7d1262be4e761eef91224278497f0e
codgm <sgm0209@naver.com>
2024-09-03 16:52:31 +0900

    fix 로그인 화면에 네이버 및 구글 로그인 수정

--- app/src/main/java/com/example/mappin_fe/Login_Sign/GlobalApplication/GlobalApplication.kt
+++ app/src/main/java/com/example/mappin_fe/Login_Sign/GlobalApplication/GlobalApplication.kt
@@ -1,6 +1,7 @@
 package com.example.mappin_fe.Login_Sign.GlobalApplication
 
 import android.app.Application
+import com.example.mappin_fe.R
 import com.kakao.sdk.common.KakaoSdk
 import com.navercorp.nid.NaverIdLoginSDK
 
@@ -8,7 +9,7 @@ class GlobalApplication : Application() {
     override fun onCreate() {
         super.onCreate()
 
-        KakaoSdk.init(this, "607b30fba1f577a8f048920ea64a6d50")
+        KakaoSdk.init(this, getString(R.string.kakao_app_key))
 
         // Naver SDK 초기화
         NaverIdLoginSDK.initialize(this, "dXtaisFDzZtaLMIyRRtW", "b7cyVWDy7b", "Mappin")
